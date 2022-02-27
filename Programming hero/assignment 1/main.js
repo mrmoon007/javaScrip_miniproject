4f182f634311fe6b2f17eed1479ddb8d600a2ccc
mrmoon007 <mrmoon025@gmail.com>
2022-02-27 15:35:38 +0600

    tiny friend problem slove

--- Programming hero/assignment 1/main.js
+++ Programming hero/assignment 1/main.js
@@ -5,3 +5,16 @@ function feetToMile( feet ){
 
 let mile = feetToMile(70000);
 console.log(mile);
+
+function tinyFriend( friends ) {
+    let result = friends[0];
+    for(let i =0; i< friends.length ; i++) {
+        let y = friends[i];
+        if (y.length < result.length) {
+            result= friends[i];
+        }
+    }
+    return result;
+}
+
+console.log(tinyFriend(['moon0','mostafijur','hemal']))
