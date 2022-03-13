b363044b0242372d5b6ac1bdfb451d9e5bffe9aa
mrmoon007 <mrmoon025@gmail.com>
2022-03-13 12:01:29 +0600

    vue project

---
+++ vue/project/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+
+  <FooterMoon />
+
+</template>
+
+<script>
+// import HelloMoon from './components/HelloMoon.vue' 
+import FooterMoon from './components/FooterMoon.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloMoon,
+    FooterMoon
+  },
+  data(){
+    return {
+      fname: "Mostafijur",
+      name : "I am from app component",
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/project/src/components/HelloMoon.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+    <h3>i am from header</h3>
+    <p>{{ name }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+      name,
+  }
+}
+</script>
+
+<style>
+
+</style>
